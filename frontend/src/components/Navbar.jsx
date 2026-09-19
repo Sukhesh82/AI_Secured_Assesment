6ddf0fb636394f57979b4c8b5e680152174e6087
Sukhesh18 <23h41a0582@bvcits.edu.in>
2026-09-19 15:58:41 +0530

    Make Navbar full width to align logo with sidebar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
   return (
     <header className="sticky top-0 z-40 bg-bg-dark border-b border-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
+      <div className="w-full px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
         
         {/* Brand Logo */}
         <Link to="/" className="flex items-center gap-3 group">
